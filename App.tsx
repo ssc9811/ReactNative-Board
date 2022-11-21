129b5cd2c4bc5b45401ce9bcdcdf038420e37ad8
ssc9811 <poer2418@naver.com>
2022-11-21 17:44:51 +0900

    init

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View``;
+
+const App = () => {
+  return (
+    <View>
+      <Text>임시</Text>
+    </View>
+  );
+};
+
+export default App;
